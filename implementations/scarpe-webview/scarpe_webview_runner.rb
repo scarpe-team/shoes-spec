4d831b1fdcfe8df6d03778f3333b6a7488279c34
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-10-27 20:33:25 +0100

    Fix for latest Scarpe Shoes-Spec changes, and to handle multiple specs much better

--- implementations/scarpe-webview/scarpe_webview_runner.rb
+++ implementations/scarpe-webview/scarpe_webview_runner.rb
@@ -1,19 +1,59 @@
+# frozen_string_literal: true
+
+require "rbconfig"
+require "json"
+
+require "scarpe/components/file_helpers"
+require_relative "../../lib/shoes-spec/test_list"
+require_relative "../../lib/shoes-spec/shoes_config"
+
 module Scarpe
   module Webview
     module Runner
-      def run_scarpe_command_line metadata, app_code, test_code
+      include ShoesSpec
+      include Scarpe::Components::FileHelpers
+
+      def run_all_tests
+        results = []
+        with_each_loaded_test(display_service: "scarpe-webview") do |metadata, app_code, test_code|
+          results << run_scarpe_command_line_test(metadata, app_code, test_code)
+        end
+        results
+      end
+
+      def run_scarpe_command_line_test metadata, app_code, test_code
         with_tempfiles(
           [
             ["shoes-spec-scarpe-test-app", app_code],
             ["shoes-spec-scarpe-test-code", test_code],
           ]) do |app_file, test_code_file|
+            sspec_file = File.expand_path "sspec.json"
+
             # TODO: env vars SCARPE_LOG_CONFIG, SCARPE_TEST_RESULTS, LOCALAPPDATA=Dir.tmpdir
             ENV["SHOES_SPEC_TEST"] = test_code_file
-            ENV["SHOES_MINITEST_EXPORT_FILE"] = "sspec.json"
+            ENV["SHOES_MINITEST_EXPORT_FILE"] = sspec_file
             ENV["SHOES_MINITEST_CLASS_NAME"] = metadata["category"].gsub("/", "_")
-            ENV["SHOES_MINITEST_METHOD_NAME"] = metadata["test_name"]
+            ENV["SHOES_MINITEST_METHOD_NAME"] = metadata["test_name"].gsub(".sspec", "")
             system(RbConfig.ruby, which("scarpe"), "--dev", app_file)
+            return File.read(sspec_file)
         end
+      end
+
+      def minitest_run_with_imported_results(results)
+        require "scarpe/components/minitest_import_runnable"
+        results.each do |res|
+          Scarpe::Components::ImportRunnables::ImportRunnable.import_json_data(res)
+        end
+      end
+
+      def minitest_runner_process
+        pid = fork do
+          require "minitest/autorun"
+          results = run_all_tests
+          minitest_run_with_imported_results(results)
+        end
+
+        pid
       end
     end
   end
