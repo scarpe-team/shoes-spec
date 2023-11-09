bd3acae5e10c42b7f9d4803587870a7c4427a1c1
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-11-09 19:56:04 +0000

    Add initial runner harness and (bad) results for Niente, the do-nothing display service

---
+++ implementations/niente/scarpe_niente_runner.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "rbconfig"
+require "json"
+
+require "scarpe/components/file_helpers"
+require_relative "../../lib/shoes-spec/test_list"
+require_relative "../../lib/shoes-spec/shoes_config"
+require_relative "../../lib/shoes-spec/report_results"
+
+module Niente
+  module Runner
+    include ShoesSpec
+    include Scarpe::Components::FileHelpers
+
+    def run_scarpe_command_line_test metadata, app_code, test_code
+      with_tempfiles(
+        [
+          ["shoes-spec-scarpe-test-app", app_code],
+          ["shoes-spec-scarpe-test-code", test_code],
+        ]) do |app_file, test_code_file|
+          sspec_file = File.expand_path "sspec.json"
+
+          # TODO: LOCALAPPDATA=Dir.tmpdir
+          ENV["SHOES_SPEC_TEST"] = test_code_file
+          ENV["SCARPE_DISPLAY_SERVICE"] = "niente"
+          ENV["SHOES_MINITEST_EXPORT_FILE"] = sspec_file
+          ENV["SHOES_MINITEST_CLASS_NAME"] = metadata["category"].gsub("/", "_")
+          ENV["SHOES_MINITEST_METHOD_NAME"] = metadata["test_name"].gsub(".sspec", "")
+          system(RbConfig.ruby, which("scarpe"), "--dev", app_file)
+          return File.read(sspec_file)
+      end
+    end
+
+    def report_niente_specs
+      report = ShoesSpec::ReportResults.new(display: "niente", config: "local")
+
+      with_each_loaded_test(display_service: "niente") do |metadata, app_code, test_code|
+        res = JSON.load run_scarpe_command_line_test(metadata, app_code, test_code)
+        unless res.is_a?(Array) && res.size == 1
+          raise "Internal error! Unexpected result format from run_scarpe_command_line_test!"
+        end
+        res = res[0]
+
+        test_name = metadata["test_name"]
+        category = metadata["category"]
+        if res["failures"].empty?
+          report.report(:pass, test_name:, category:)
+        else
+          report.report(:fail, test_name:, category:)
+        end
+      end
+
+      path = report.complete
+      puts "Wrote ShoesSpec results to #{path}"
+      compare_results(display: "niente", config: "local")
+    end
+  end
+end
